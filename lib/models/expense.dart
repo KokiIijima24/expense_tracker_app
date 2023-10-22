50b6d12190c6c8cb9ba1360588ca20f50010bc58
KoukiIijima24 <koki.iijima@jbs.com>
2023-10-22 14:25:01 +0900

    [add] Expense Category enum, and field

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,13 +1,23 @@
 import 'package:uuid/uuid.dart';
 
-final uuid = Uuid();
+const uuid = Uuid();
+
+enum Category {
+  leisure,
+  food,
+  travel,
+  work,
+  other,
+}
 
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category; // leisure Expense(vategory: 'leasute')
 
-  Expense({required this.title, required this.amount, required this.date})
+  Expense(this.category,
+      {required this.title, required this.amount, required this.date})
       : id = uuid.v4();
 }
